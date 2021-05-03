3331791e1c796e4fbcba6a776b1891a9bc016c89
manoj-40119 <manojkumarbonu1999@gmail.com>
2021-05-03 09:41:48 +0530

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-app');
+    expect(app.name).toEqual('angular-app');
   });
 
   it('should render title', () => {
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { CssSelector } from '@angular/compiler';
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'server-app',
+  templateUrl: './server.component.html',
+  styleUrls:['./server.component.css']
+})
+
+
+export class ServerComponent{
+
+}
