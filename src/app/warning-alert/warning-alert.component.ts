b272456a0588f78454910334591e34aea93a3891
manoj-40119 <manojkumarbonu1999@gmail.com>
2021-05-04 17:59:18 +0530

    commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-warning-alert',
+  template:'<p> WARNING</p>',
+  styles : [
+    'p { padding: 20px ; background-color: mistyrose;  border: 1px solid red; }']
+
+})
+export class WarningAlertComponent{
+
+}
