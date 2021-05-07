bbc891262a30ac8e00413103b76b435669ef4cb9
manoj-40119 <manojkumarbonu1999@gmail.com>
2021-05-07 10:54:30 +0530

    commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,6 +9,9 @@ import { Component} from '@angular/core';
 export class ServerComponent{
      serverId=10;
      serverStatus = 'offline';
+     constructor(){
+       this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+     }
 
      getserverStatus(){
        return this.serverStatus;
